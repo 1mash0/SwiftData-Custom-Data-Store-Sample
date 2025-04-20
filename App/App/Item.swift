49f828ad830e87a76480eb2d63bb0de9b8936399
1mash0 <sho.response1114@gmail.com>
2025-04-20 21:15:06 +0900

    Codable削除

--- App/App/Item.swift
+++ App/App/Item.swift
@@ -2,7 +2,7 @@ import Foundation
 import SwiftData
 
 @Model
-class Item: Codable {
+class Item {
     #Unique<Item>([\.id])
     
     var id: UUID
@@ -11,22 +11,5 @@ class Item: Codable {
     init() {
         self.id = UUID()
         self.timestamp = Date()
-    }
-    
-    enum CodingKeys: CodingKey {
-        case id
-        case timestamp
-    }
-    
-    required init(from decoder: any Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(UUID.self, forKey: .id)
-        timestamp = try container.decode(Date.self, forKey: .timestamp)
-    }
-    
-    func encode(to encoder: any Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(timestamp, forKey: .timestamp)
     }
 }
